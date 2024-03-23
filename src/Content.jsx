ffe0cb470cb848160acdc0bf685a22e744b3cfb9
samirvithlani <samir.vithlani83955@gmail.com>
2024-03-23 14:56:44 +0530

    map

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,6 +3,22 @@ import { SubContent } from './SubContent';
 
 export const Content = (props) => {
 
+
+  const users = [
+    {
+      id:1,
+      name:"amit",
+      age:23
+    },
+    {
+      id:2,
+      name:"raj",
+      age:24
+    }
+  ]
+
+
+
 console.log("props in content....",props);
   return (
     <div>
@@ -11,6 +27,20 @@ console.log("props in content....",props);
         </h1>
         <h2>{props.data.id}</h2>
         <h2>{props.data.name}</h2>
+        {
+          users.map((user)=>{
+            return(
+            <ul>
+              <li>{user.id}</li>
+              <li>{user.name}</li>
+              <li>{user.age}</li>
+            </ul>
+            )
+          })
+        }
+
+
+
 
         <SubContent data = {props.data}></SubContent>
     </div>
